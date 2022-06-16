09df3624c09cf27e6a50ce727a1da6f4c07dc378
matheus-rosa11 <matheus.rosa@sptech.school>
2022-06-16 00:06:37 -0300

    :feat-fix: adding images to artist albums on index and fixing header button hovers

--- site/src/database/script-tabelas.sql
+++ site/src/database/script-tabelas.sql
@@ -41,6 +41,15 @@ create table medida (
 	FOREIGN KEY (fk_aquario) REFERENCES aquario(id)
 );
 
+insert into artista(nomeArtista, imgAlbum) values
+	('The Beatles', 'https://i.discogs.com/hjWXIS3HPxmW-XsWlmqznmmGR5FGsL-scfwWFgxL4yU/rs:fit/g:sm/q:90/h:562/w:567/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9BLTgyNzMw/LTEzMTk1MzIzMzEu/anBlZw.jpeg'),
+	('Ed Sheeran', 'https://i.discogs.com/iLcqGPjykDh1s4WCnPEWpD-VyvrpLBhWsVGq09tWF14/rs:fit/g:sm/q:90/h:400/w:400/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9SLTU4MzA4/MDYtMTQwMzg5NDU5/MS01NjA5LmpwZWc.jpeg'),
+	('Shawn Mendes', 'https://i.discogs.com/iMK0m5hrXd5dnrZv6UMF267DwSTyR42zfyR5mfDayWM/rs:fit/g:sm/q:90/h:546/w:600/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9SLTEyMTg1/ODU3LTE2MTk3Njgw/MDEtNTk0My5qcGVn.jpeg'),
+	('Billie Eilish', 'https://i.discogs.com/fuqdnfhlfcknjJ2BhWw1b2Y9d3Lo5AtFqeTQfGPjuRc/rs:fit/g:sm/q:90/h:537/w:600/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9SLTE5Njkz/OTMzLTE2Mjg5ODAx/NDItMjM2MC5qcGVn.jpeg'),
+	('Post Malone', 'https://i.discogs.com/hPsU-pkwR8d2Jjj9mDmlQJyBQfKIu3rPeon32xY4H2c/rs:fit/g:sm/q:90/h:695/w:600/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9BLTQwMTY0/MzQtMTU0NjY2MTcz/Mi0zMjI4LmpwZWc.jpeg'),
+	('Justin Bieber', 'https://i.discogs.com/MkaVNDn2huuA2XqWDRQyLH3jjML7DiXzuzZo_lOfABo/rs:fit/g:sm/q:90/h:701/w:500/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9BLTE2MDI3/ODctMTUxNTI3ODEy/NC04Mjc0LmpwZWc.jpeg');
+
+
 
 /* para sql server - remoto - produção */
 CREATE TABLE usuario (
